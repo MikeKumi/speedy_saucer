1144e55e2565824459fa5dfd56561d92d43046f1
MikeKumi <michael.kumicich@valpo.edu>
2024-12-21 00:51:34 -0600

    Added controls to player

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,11 +1,21 @@
 extends RigidBody2D
 
+var FORCE = 500
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+# Cares about global framerate
 func _process(delta):
 	pass
+
+# Does not care about global framerate
+func _physics_process(delta):
+	if Input.is_action_pressed("move_right"):
+		apply_force(Vector2(FORCE, 0))
+	if Input.is_action_pressed("move_left"):
+		apply_force(Vector2(-FORCE, 0))
+	if Input.is_action_pressed("move_up"):
+		apply_force(Vector2(0, -FORCE))
+	if Input.is_action_pressed("move_down"):
+		apply_force(Vector2(0, FORCE))
